bf672f55fd01eef7c066cc45d52f1f055181e72f
Iqra Fayyaz <iqrafayyaz0@gmail.com>
2026-03-30 23:08:52 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Formula from "./components/Formula";
+import Formulas from "./components/Formulas";
+
+export default function App(){
+  return(
+    <>
+    <h1>Mathmaticals Formula</h1>
+    <Formula/>
+    <Formulas/>
+    </>
+  );
+}
---
+++ src/components/Formula.jsx
@@ -0,0 +1,10 @@
+export default function Formula(){
+  return(
+    <>
+    <p>
+    <em>(a + b + c)<sup>3</sup> = a<sup>3</sup> + b<sup>3</sup> + c<sup>3</sup> + 6abc + 3(a<sup>2</sup>b + ab<sup>2</sup> + b<sup>2</sup>c + bc<sup>2</sup> + c<sup>2</sup>a + ca<sup>2</sup>)
+</em></p>
+    </>
+    
+  );
+}
